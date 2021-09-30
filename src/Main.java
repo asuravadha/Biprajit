74856ad6fd5f33ecfe16b538aaf2892200db7b66
asuravadha <rr.easwar@gmail.com>
2021-09-30 19:59:07 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+
+//        File obj=new File("data.txt");
+//        Scanner input= null;
+//        try {
+//            input = new Scanner(obj);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        while(input.hasNextLine()){
+//            String data=input.nextLine();
+//            System.out.println(data);
+//        }
+
+        //        String[] names=new String[10];
+//        Scanner input=new Scanner(System.in);
+//
+//        for(int i=0;i<names.length;i++){
+//            System.out.println("Enter your name");
+//           names[i]=input.next();
+//        }
+//System.out.println("Values are");
+//        for(String name:names){
+//            System.out.println(name);
+//        }
+    }
+}
