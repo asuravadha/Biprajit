39eef346a1018d8bd1f728c89a86e9acf206927a
asuravadha <rr.easwar@gmail.com>
2021-09-30 20:35:27 +0530

    array grid

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         arrays arr=new arrays();
+        int r=0,c=0;
+        arr.data("reset",r,c);
+        Scanner input=new Scanner(System.in);
+        while(true){
+            System.out.println("enter u,d,l,r or anyother to quit");
+            String option=input.next();
+            if(option.equals("u")){
+                r--;
+            }
+            else if(option.equals("d")){
+                r++;
+            }
+            else if(option.equals("l")){
+                c--;
+            }
+            else if(option.equals("r")){
+                c++;
+            }
+            else{
+                break;
+            }
+            arr.data("",r,c);
+        }
 
 //        File obj=new File("data.txt");
 //        Scanner input= null;
--- src/arrays.java
+++ src/arrays.java
@@ -1,6 +1,25 @@
 public class arrays {
-        arrays(){
-            String[][] data={{"a","b","c"},{"e","f","g"}};
+    public  void data(String input,int r,int c){
+        String[][] data=new String[3][3];
+        if(input.equals("reset")){
+            r=0;
+            c=0;
+           for(int i=0;i<3;i++){
+               for(int j=0;j<3;j++){
+                   data[i][j]="*";
+               }
+           }
+           data[r][c]="^";
+        }
+        else{
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    data[i][j]="*";
+                }
+            }
+            data[r][c]="^";
+
+        }
             for(String[] i: data){
                 for(String j: i){
                     System.out.print(j+"  ");
