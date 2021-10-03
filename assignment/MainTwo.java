fc43fe0c2fdf534ca538046d0a5d462e449b7671
Biprajit Majumder <biprajit163@gmail.com>
2021-10-03 17:37:30 -0400

    signup feature working

---
+++ assignment/MainTwo.java
@@ -0,0 +1,36 @@
+
+import features.Signup;
+
+import java.util.Scanner;
+
+public class MainTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Signup signObj = new Signup();
+
+        String username, password, answer;
+        boolean checkSignup = true;
+
+        while(checkSignup == true) {
+            System.out.println("Would you like to signup? yes or no");
+            answer = sc.nextLine();
+
+            if(answer.equals("yes")) {
+                checkSignup = true;
+
+                System.out.println("Enter a username: ");
+                username = sc.nextLine();
+
+                System.out.println("Enter a password: ");
+                password = sc.nextLine();
+
+                signObj.addUser(username, password);
+            } else if(answer.equals("no")) {
+                System.out.println("Thank you for signing up! ");
+                checkSignup = false;
+            }
+        }
+
+        signObj.getAllUsers();
+    }
+}
